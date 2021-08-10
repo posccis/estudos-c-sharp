30e3af804fca4d6c894e38362ed554249e815df9
Posccis <lkzq33@gmail.com>
2021-08-09 23:34:43 -0300

    Exercicio de CRUD com ASP.NET MVC - Uma Lista de livros

---
+++ BookListRazor/BookListRazor/wwwroot/js/bookList.js
@@ -0,0 +1,41 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable;
+})
+
+function loadDataTable() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/Book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "titulo", "width": "30%" },
+            { "data": "autor", "width": "30%" },
+            { "data": "isbn", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                    <a href="/BookList/Edit?id=${data}" class'btn btn-info text-white' style='cursor:pointer; width: 100px'>
+                        Edit
+                    </a>
+                    &nbsp;
+                    <a class'btn btn-info text-white' style='cursor:pointer; width: 100px'>
+                        Delete
+                    </a>
+
+                    </div>`;
+
+
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found"
+        },
+        "width": "100%"
+    });
+}
---
+++ BookListRazor/readme.md
@@ -0,0 +1,30 @@
+<h1>Pagina inicial</h1>
+
+<p>Primeira prática de Asp.Net MVC</p>
+
+<p>Seguindo o curso da FreeCodeCamp.org, desenvolvi um aplicativo basico de CRUD(Creat, Read, Update e Delete) ligado a um banco de dados SQL.</p>
+
+
+
+<img src="/BookListRazor/home.png">
+
+<hr>
+
+<h4>Pagina com a lista de livros</h3>
+
+<img src="/BookListRazor/Pagina_booklist.png">
+
+<hr>
+
+<h4>Pagina para inserir os livros</h3>
+
+<img src="/BookListRazor/Pagina_inserir.png">
+
+<hr>
+
+<h4>Pagina para editar os livros</h3>
+
+<img src="/BookListRazor/Pagina_edit.png">
+
+
+
